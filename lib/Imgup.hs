e09b2589d0c6c775dc91af92b3458c50a2c489ae
brady.ouren <brady.ouren@gmail.com>
2016-05-30 19:05:48 -0700

    add recent screenshot upload flag

--- lib/Imgup.hs
+++ lib/Imgup.hs
@@ -4,31 +4,56 @@ module Imgup where
 
 import qualified Configuration.Dotenv as Dotenv
 import Control.Lens
-import Data.Aeson
 import Data.Aeson.Lens
 import qualified Data.ByteString as BS
-import Data.Text as T
+import Data.List (sortBy)
+import qualified Data.Text as T
 import Data.Text.Encoding (encodeUtf8)
 import Network.Wreq
 import System.Directory (getCurrentDirectory, getHomeDirectory)
 import System.Environment (getArgs, getEnv)
 import System.FilePath (joinPath)
+import System.FilePath.Glob (compile, globDir1)
 
 imgupConfigPath = do
   homePath <- getHomeDirectory
   return (joinPath [homePath, ".imgup"])
 
+clientId :: IO BS.ByteString
 clientId = do
   Dotenv.loadFile True =<< imgupConfigPath
   cid <- getEnv "CLIENT_ID"
   return $ encodeUtf8 $ T.pack cid
 
+headMaybe :: [a] -> Maybe a
+headMaybe (x:_) = Just x
+headMaybe []  = Nothing
+
 imagePath path = do
   d <- getCurrentDirectory
   return $ joinPath [d, path]
 
+desktopPath = do
+  homePath <- getHomeDirectory
+  return (joinPath [homePath, "Desktop"])
+
+getRecentPath = do
+  d <- desktopPath
+  files <- globDir1 (compile "Screen Shot*") d
+  case headMaybe (sortBy (flip compare) files) of
+    Nothing -> error "no recent screenshots"
+    Just a -> return a
+
+parseArgs = do
+  args <- getArgs
+  case args of
+    ["--screenshot"] -> getRecentPath
+    (path:_)           -> return path
+    _                  -> error "invalid args"
+
+uploadAndReturnUrl :: IO String
 uploadAndReturnUrl = do
-  (path:_) <- getArgs
+  path <- parseArgs
   image <- imagePath path
   cid <- clientId
   let authHeader = defaults & header "Authorization" .~ [BS.intercalate " " ["Client-ID", cid]]
@@ -37,5 +62,5 @@ uploadAndReturnUrl = do
              ]
 
   r <- postWith authHeader "https://api.imgur.com/3/image.json" opts
-  let id = r ^. responseBody . key "data" . key "id" . _String
-  return $ "http://imgur.com/" ++ T.unpack id
+  let guid = r ^. responseBody . key "data" . key "id" . _String
+  return $ "http://imgur.com/" ++ T.unpack guid
