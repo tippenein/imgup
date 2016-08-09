0586d1e7c3e53c8cfdd014a7bf2341ceb0dd0ff9
brady.ouren <brady.ouren@gmail.com>
2016-08-09 00:24:01 -0700

    add upload-from-url and improve output format

--- README.md
+++ README.md
@@ -3,19 +3,33 @@ imgup
 
 Upload an image anonymously to imgur
 
+### Screenshots
+Upload your most recent screenshot anonymously with the `--screenshot` flag
+
+```
+imgup --screenshot |pbcopy
+```
+
+![screenshot example](screenshot_example.png)
+
+As of now this requires `Screen Shot*` as the naming convention _AND_ a 24 hour clock.
+
+### From Your Filesystem
+```
+imgup ~/Desktop/pepe-happy.png
+```
+
+### Reuploaded to Imgur
+```
+imgup -u http://some.crap.wordpress/whatever.gif
+```
+
 returns a url to the anonymous photo upload
 
 pipe it through `xsel` or `pbcopy` for auto copy to clipboard
 
 `imgup sloth.png | pbcopy`
 
-or you can upload your most recent screenshot anonymously with the `--screenshot` flag
-
-`imgup --screenshot |pbcopy`
-
-![screenshot example](screenshot_example.png)
-
-As of now this requires `Screen Shot*` as the naming convention _AND_ a 24 hour clock.
 
 Setup
 ===
--- lib/Imgup.hs
+++ lib/Imgup.hs
@@ -6,6 +6,7 @@ import qualified Configuration.Dotenv as Dotenv
 import Control.Lens
 import Data.Aeson.Lens
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as LBS
 import Data.List (sortBy)
 import Data.Monoid
 import qualified Data.Text as T
@@ -13,33 +14,40 @@ import Data.Text.Encoding (encodeUtf8)
 import Network.Wreq
 import System.Directory (getCurrentDirectory, getHomeDirectory)
 import System.Environment (getArgs, getEnv)
-import System.FilePath (joinPath)
+import System.FilePath (joinPath, takeExtension, takeFileName)
 import System.FilePath.Glob (compile, globDir1)
-
-imgupConfigPath :: IO FilePath
-imgupConfigPath = do
-  homePath <- getHomeDirectory
-  return (joinPath [homePath, ".imgup"])
 
 clientId :: IO BS.ByteString
 clientId = do
-  Dotenv.loadFile True =<< imgupConfigPath
+  Dotenv.loadFile True =<< fromHome ".imgup"
   cid <- getEnv "CLIENT_ID"
   return $ encodeUtf8 $ T.pack cid
 
+fromHome :: String -> IO FilePath
+fromHome p = do
+  homePath <- getHomeDirectory
+  return (joinPath [homePath, p])
+
 getRecentPath :: IO FilePath
 getRecentPath = do
-  home <- getHomeDirectory
-  d <- return (joinPath [home, "Desktop"])
+  d <- fromHome "Desktop"
   files <- globDir1 (compile "Screen Shot*") d
   case headMaybe (sortBy (flip compare) files) of
     Nothing -> error "no recent screenshots"
     Just a -> return a
 
+fromUrl :: String -> IO String
+fromUrl u  = do
+  let fileName = "/tmp/" ++ takeFileName u
+  r <- get u
+  LBS.writeFile fileName (r ^. responseBody)
+  pure fileName
+
 parseArgs :: IO FilePath
 parseArgs = do
   args <- getArgs
   case args of
+    ["-u", u] -> fromUrl u
     ["--screenshot"]   -> getRecentPath
     (path:_)           -> do
         d <- getCurrentDirectory
@@ -57,7 +65,7 @@ uploadAndReturnUrl = do
 
   r <- postWith authHeader "https://api.imgur.com/3/image.json" opts
   let guid = r ^. responseBody . key "data" . key "id" . _String
-  return $ "http://imgur.com/" ++ T.unpack guid
+  return $ "http://i.imgur.com/" ++ T.unpack guid ++ takeExtension imagePath
 
 headMaybe :: [a] -> Maybe a
 headMaybe (x:_) = Just x
